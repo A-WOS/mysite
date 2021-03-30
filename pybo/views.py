7356bee0a5237a966f07fa87d6ee2b4797518899
Awos <uws0331@gmail.com>
2021-03-30 19:09:35 +0900

    answer_create 함수 생성

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Question
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from .models import Question, Answer
 
 
 def index(request):
@@ -11,7 +13,23 @@ def index(request):
 
 
 def detail(request, question_id):
-    #question = Question.objects.get(id=question_id)
+    # question = Question.objects.get(id=question_id)
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'pybo/question_detail.html', context)
+
+
+def answer_create(request, question_id):
+    """
+    pybo 답변등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    question.answer_set.create(content=request.POST.get('cont'), create_date=timezone.now())
+
+    # 다른 방법
+    # answer = Answer(question=question, content=request.POST.get('cont'), create_date=timezone.now())
+    # answer.save()
+    # ---------------------------------- [edit] ---------------------------------- #
+    return redirect('pybo:detail', question_id=question.id)
+    # ---------------------------------------------------------------------------- #
+
