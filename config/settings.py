fc6ae3372c3210ac68be8c6397d84e76f6e8634e
Awos <uws0331@gmail.com>
2021-06-04 18:13:44 +0900

    cart 구매 더하는 기능까지 만듬

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'product.apps.ProductConfig',
     'django_summernote',
-    'order.apps.OrderConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
--- order/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from order.models import Order
-
-
-class OrderAdmin(admin.ModelAdmin):
-    list_display = ('user', 'product', 'quantity')
-
-
-admin.site.register(Order, OrderAdmin)
