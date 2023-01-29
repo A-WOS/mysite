abdbfc9c24ba22859c767582edd3f7bd97a85527
AWOS <uws0331@gmail.com>
2023-01-30 06:28:32 +0900

    static 디렉토리 ignore 처리

--- common/views.py
+++ common/views.py
@@ -13,6 +13,7 @@ def signup(request):
             form.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
+            # print(username, type(raw_password))
             user = authenticate(username=username, password=raw_password)
             login(request, user)
             return redirect('index')
